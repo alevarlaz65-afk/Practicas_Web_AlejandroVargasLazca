5d77b19cee586599b9ee4c6161d0f934e339e165
alevarlaz65-afk <alevarlaz65@gmail.com>
2026-01-22 22:12:52 -0600

    practica 4

---
+++ practica04-angular/src/app/app.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
+
+@Component({
+selector: 'app-root',
+standalone: true,
+imports: [RouterOutlet, Header, Navbar, Footer],
+templateUrl: './app.html'
+})
+export class App {
+  protected readonly title = signal('practica04-angular');
+}
